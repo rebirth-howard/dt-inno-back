45b675c31ab593fb780b385e2f940c0e3b4b3d58
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2024-10-14 00:12:09 +0900

    Swagger UI 기초 설정

---
+++ module-api/src/main/java/com/deutschmotors/moduleapi/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.deutschmotors.moduleapi.swagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("This is a sample Spring Boot RESTful service using Springdoc OpenAPI and OpenAPI 3."));
+    }
+}
