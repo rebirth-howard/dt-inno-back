ef16d401a5589c6666763c1f5ddb35871fe00654
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2024-10-14 02:54:09 +0900

    공통 filter 및 Interceptor 기본 셋팅

--- module-api/src/main/java/com/deutschmotors/moduleapi/config/swagger/SwaggerConfig.java
+++ module-api/src/main/java/com/deutschmotors/moduleapi/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.deutschmotors.moduleapi.swagger;
+package com.deutschmotors.moduleapi.config.swagger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
